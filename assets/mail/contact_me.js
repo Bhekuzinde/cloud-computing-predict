810d0ac0243a037215eaed6ed9395d51835de733
Bhekuzinde <52311010+Bhekuzinde@users.noreply.github.com>
2023-10-15 16:35:37 +0200

    Update contact_me.js
    updated API version

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://sud6dwn4ij.execute-api.eu-west-1.amazonaws.com",
+                url: "https://tb326oafej.execute-api.eu-west-1.amazonaws.com/default/AnalyzeSentiment",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
